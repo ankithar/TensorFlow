1325ce4bcd5da0538df4dc58bb46cdfa47eb7651
ankithar <ankithar.12@gmail.com>
2019-04-27 07:50:38 -0700

    Linear regression for linearly spaced data

---
+++ LinearRegression/LinearRegression.py
@@ -0,0 +1,46 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib
+matplotlib.use('TKAgg')
+from matplotlib import pyplot as plt
+
+def generate_dataset():
+    x_batch = np.linspace(-1,1,101)
+    y_batch  =2 * x_batch + np.random.randn(*x_batch.shape) * 0.3
+    return x_batch, y_batch
+
+def linear_regression():
+    x = tf.placeholder(tf.float32,shape=(None,),name='x')
+    y = tf.placeholder(tf.float32, shape=(None,), name='y')
+
+    #with tf.variable_scope('lreg') as scope:
+    w = tf.Variable(np.random.normal(),name='W')
+    y_pred =  tf.multiply(w,x)
+    loss = tf.reduce_mean(tf.square(y_pred-y))
+    return  x,y,y_pred,loss
+
+def runPgm():
+    x_batch, y_batch =  generate_dataset()
+
+    x, y, y_pred, loss = linear_regression()
+    optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+
+        feed_dict = {x: x_batch, y:y_batch}
+        for _ in range(100):
+            loss_val,_ = sess.run([loss,optimizer],feed_dict)
+            print("loss : ",loss_val)
+
+        y_pred_batch = sess.run(y_pred,  {x:x_batch})
+
+    plt.figure(1)
+    plt.scatter(x_batch,y_batch)
+    plt.plot(x_batch,y_pred_batch)
+    plt.savefig('plot.png')
+
+
+if __name__=='__main__':
+    runPgm()
